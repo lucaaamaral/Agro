d167e3f079e437581d7644e08924fdc65072c197
lucaaamaral <lucaaamaral@gmail.com>
2020-05-12 18:06:24 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "implem.cpp"
+
+
+
+int main(int argc, char *argv[])
+{
+	lock(50);
+	int fd = open("/dev/mem", O_RDWR);
+	mapa = (int*)mmap(0, GPIO0_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO0_START);
+	if(mapa == MAP_FAILED) 
+	{
+			close(fd);
+			perror("Unable to map /dev/mem");
+			exit(EXIT_FAILURE);
+	}
+	
+	
+	int dir = *(mapa+OE);
+	printf("%d", dir);
+	
+	
+	pthread_t t;
+	pthread_create(&t, 0, monitor, 0);
+	
+	sComm(argc, argv);
+	pthread_join(t, 0);
+	
+	close(fd);
+	munmap(mapa, GPIO0_LEN);
+	
+	printf("Beaware of time delays, they might kill your application\n\n");
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
